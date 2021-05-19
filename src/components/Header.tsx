d4357eb6a61b4cf2769b7364f3e1603b296dd445
patrick humphries <patrickhumphries95@gmail.com>
2021-05-19 15:25:44 -0400

    added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <div className="Header">
+        <input type="text" placeholder="Search by keyword..." />
+        <button type="submit">Search</button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/service/events-service.ts
@@ -0,0 +1,1 @@
+export const apiKey = process.env.REACT_APP_EVENTS_API_KEY || '';
